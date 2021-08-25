b77bc4b0664294a749bdfc523763cb4ba38bda64
Damian Krawczyk <damian.krawczyk@limberduck.org>
2021-08-25 21:22:05 +0200

    v. 0.0.2

--- tsccm/modules/tscapi/__init__.py
+++ tsccm/modules/tscapi/__init__.py
@@ -1,58 +1,68 @@
 from tenable.sc import TenableSC
 import certstore
+import urllib3
 
 
 class TscApi:
-    sc_addr = "127.0.0.1"
-    verify = certstore.ca_bundle
 
-    def __init__(self, sc_addr, verify):
-        self.sc_addr = sc_addr
-        self.verify = verify
-        self.sc = TenableSC(sc_addr, ssl_verify=verify)
+    def __init__(self, host='127.0.0.1', port=443, insecure=None):
+        self.host = host
+        self.port = port
+        if not insecure:
+            self.verify = certstore.ca_bundle
+        else:
+            self.verify = False
+            urllib3.disable_warnings()
+
+        self.sc = TenableSC(self.host, self.port, ssl_verify=self.verify)
 
     def login(self, sc_user, sc_pass):
         self.sc.login(sc_user, sc_pass)
 
-    def status(self):
+    def logout(self):
+        self.sc.logout()
+
+    def status_get(self):
         response = self.sc.get('status')
         response_json = response.json()
         return response_json
 
-    def system(self):
+    def system_get(self):
         response = self.sc.get('system')
         response_json = response.json()
         return response_json
 
-    def logout(self):
-        self.sc.logout()
-
     def user_get(self):
-        response = self.sc.get('user?fields=id,username,state')
+        response = self.sc.get('user?fields=id,username,firstname,lastname,role,createdTime,modifiedTime,lastLogin')
         response_json = response.json()
         return response_json
 
     def group_get(self):
-        response = self.sc.get('group?fields=id,name')
+        response = self.sc.get('group?fields=id,name,createdTime,modifiedTime')
         response_json = response.json()
         return response_json
 
     def scan_get(self):
-        response = self.sc.get('scan?fields=id,name,createdTime')
+        response = self.sc.get('scan?fields=id,name,owner,createdTime,modifiedTime')
         response_json = response.json()
         return response_json
 
     def scan_results_get(self):
-        response = self.sc.get('scanResults?fields=id,name,createdTime')
+        response = self.sc.get('scanResults?fields=id,name,owner,createdTime,modifiedTime')
         response_json = response.json()
         return response_json
 
     def policy_get(self):
-        response = self.sc.get('policy?fields=id,name,createdTime')
+        response = self.sc.get('policy?fields=id,name,owner,createdTime,modifiedTime')
         response_json = response.json()
         return response_json
 
     def credential_get(self):
-        response = self.sc.get('credential?fields=id,name,createdTime')
+        response = self.sc.get('credential?fields=id,name,owner,createdTime,modifiedTime')
+        response_json = response.json()
+        return response_json
+
+    def role_get(self):
+        response = self.sc.get('role?fields=id,name,createdTime,modifiedTime,organizationCounts')
         response_json = response.json()
         return response_json
